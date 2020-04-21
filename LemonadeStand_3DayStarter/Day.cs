15889ca144233b667e94bd71c5919899d6a0a1e9
tvang83 <62636069+tvang83@users.noreply.github.com>
2020-04-21 09:19:09 -0500

    inital upload

---
+++ LemonadeStand_3DayStarter/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Day
+    {
+
+        //Main priority of this class is to provide random day variable 
+    }
+}
---
+++ LemonadeStand_3DayStarter/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    abstract class Item
+    {
+        // member variables (HAS A) - 
+        public string name;
+        public int price;
+        public int amount;
+
+        // constructor (SPAWNER)
+        public Item(string name, int price, int amount) //default variables for all shop items
+        {
+            this.name = name;
+            this.price = price;
+            this.amount = amount;
+
+        }
+
+        // member methods (CAN DO)
+
+
+    }
+}
