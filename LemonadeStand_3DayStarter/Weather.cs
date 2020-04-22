c446736928869406f2c29ca41bef365f16334ec5
tvang83 <62636069+tvang83@users.noreply.github.com>
2020-04-22 11:11:14 -0500

    update

--- LemonadeStand_3DayStarter/Weather.cs
+++ LemonadeStand_3DayStarter/Weather.cs
@@ -8,5 +8,37 @@ namespace LemonadeStand_3DayStarter
 {
     class Weather
     {
+        //member variables 
+        public int temperature;
+        public string condition;
+        public List<Customer> customers;
+
+        //constructor
+        public Weather()
+        {
+
+        }
+
+
+        //methods 
+        public int Temperature() //generate a random number between 50-100 for temperature
+        {
+            Random rand = new Random();
+            int temperature = rand.Next(51, 101);
+            return temperature;
+        }
+
+        public string Condition()
+        {
+            condition = new List<string> { "Hazy", "Cloudy", "Rainy", "Sunny", "Overcast"};
+
+            Random rand = new Random();
+            string randCondition = rand.Next(condition.Count);
+            return randCondition;
+
+        }
+
+
+
     }
 }
